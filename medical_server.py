ed63a6d6ccd61262cd95034f443776de6769d618
Hsin-lin Cheng <lancetw@gmail.com>
2013-05-05 21:52:01 +0800

    fix indent

--- medical_server.py
+++ medical_server.py
@@ -17,14 +17,14 @@ def gen_key():
 # XOR 函數
 from itertools import izip, cycle
 def XOR(key, data):
-    return ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(data, cycle(key)))
+	return ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(data, cycle(key)))
 
 # 產生裝置的 MAC address
 def gen_node_id():
 	mac = [ 0x00, 0x16, 0x3e,
-			random.randint(0x00, 0x7f),
-			random.randint(0x00, 0xff),
-			random.randint(0x00, 0xff) ]
+		random.randint(0x00, 0x7f),
+		random.randint(0x00, 0xff),
+		random.randint(0x00, 0xff) ]
 	return ':'.join(map(lambda x: "%02x" % x, mac))
 
 def rand_num_list(n, total):
