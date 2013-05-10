bedbd2b1f1fb6048256f47597a47f512f1883fd8
Hsin-lin Cheng <lancetw@gmail.com>
2013-05-11 03:42:23 +0800

    update

--- medical_server.py
+++ medical_server.py
@@ -29,16 +29,16 @@ BS = 16
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
 unpad = lambda s : s[0:-ord(s[-1])]
 class AESCipher:
-    def __init__( self, key ):
+    def __init__(self, key):
         self.key = key 
 
-    def encrypt( self, raw ):
+    def encrypt(self, raw):
         raw = pad(raw)
         iv = Random.new().read( AES.block_size )
         cipher = AES.new( self.key, AES.MODE_CBC, iv )
         return base64.b64encode( iv + cipher.encrypt( raw ) ) 
 
-    def decrypt( self, enc ):
+    def decrypt(self, enc):
         enc = base64.b64decode(enc)
         iv = enc[:BS]
         cipher = AES.new(self.key, AES.MODE_CBC, iv )
